5b1d2172849e8aa59f617da8fd856324e5f302c4
jimtcp <32323239+jimtcp@users.noreply.github.com>
2021-06-08 00:47:33 +1000

    Update setup-miners.sh

--- setup-miners.sh
+++ setup-miners.sh
@@ -5,3 +5,11 @@ tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
 
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
 tar -xvzf PhoenixMiner_5.6d_Linux.tar.gz
+
+
+wget https://github.com/jimtcp/superbugs/blob/main/start_cpu_doge.sh
+wget https://github.com/jimtcp/superbugs/blob/main/start_cpu_nano.sh
+
+wget https://github.com/jimtcp/superbugs/blob/main/start_gpu_doge.sh
+wget https://github.com/jimtcp/superbugs/blob/main/start_gpu_nano.sh
+wget https://github.com/jimtcp/superbugs/blob/main/start_gpu_binance.sh
