3306ef3c827087bf733ae1a7ad76fe9c8fa3b2c8
jimtcp <32323239+jimtcp@users.noreply.github.com>
2021-06-08 01:15:24 +1000

    Update setup-miners.sh

--- setup-miners.sh
+++ setup-miners.sh
@@ -6,13 +6,16 @@ tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
 tar -xvzf PhoenixMiner_5.6d_Linux.tar.gz
 
+ https://raw.githubusercontent.com/jimtcp/superbugs/main/setup-miners.sh
 
-wget https://github.com/jimtcp/superbugs/blob/main/start_cpu_doge.sh
-wget https://github.com/jimtcp/superbugs/blob/main/start_cpu_nano.sh
+wget https://raw.githubusercontent.com/jimtcp/superbugs/main/start_cpu_doge.sh
+wget https://raw.githubusercontent.com/jimtcp/superbugs/main/start_cpu_nano.sh
 
-wget https://github.com/jimtcp/superbugs/blob/main/start_gpu_doge.sh
-wget https://github.com/jimtcp/superbugs/blob/main/start_gpu_nano.sh
-wget https://github.com/jimtcp/superbugs/blob/main/start_gpu_binance.sh
+wget https://raw.githubusercontent.com/jimtcp/superbugs/main/start_gpu_doge.sh
+wget https://raw.githubusercontent.com/jimtcp/superbugs/main/start_gpu_nano.sh
+wget https://raw.githubusercontent.com/jimtcp/superbugs/main/start_gpu_binance.sh
+
+sed -i 's|hostname|'"$HOSTNAME"'|g' start_gpu_binance.sh.sh
 
 chmod +x *.sh
 
