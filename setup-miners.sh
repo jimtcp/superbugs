011d0dd25d447e9ce4b01452405fb64448a1563c
jimtcp <32323239+jimtcp@users.noreply.github.com>
2021-06-07 23:40:50 +1000

    Add files via upload

---
+++ setup-miners.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xvzf xmrig-6.12.1-linux-x64.tar.gz
+
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
+tar -xvzf PhoenixMiner_5.6d_Linux.tar.gz
