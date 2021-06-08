e53b904bf2ec7461666c641e8b55fe2063a3fc52
jimtcp <32323239+jimtcp@users.noreply.github.com>
2021-06-08 10:53:39 +1000

    Update start_gpu_nano.sh

--- start_gpu_nano.sh
+++ start_gpu_nano.sh
@@ -1,1 +1,1 @@
-./PhoenixMiner -pool ethash.unmineable.com:3333 -wal NANO:nano_389utniph1nbtocgrwr6kghnr55tr3wxdr9s4tsm1mxb5idig47z7mf46sbc.$HOSTNAME#xgm5-wsyf
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal NANO:nano_389utniph1nbtocgrwr6kghnr55tr3wxdr9s4tsm1mxb5idig47z7mf46sbc.hostname#xgm5-wsyf
