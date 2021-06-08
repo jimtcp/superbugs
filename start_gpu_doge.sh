30de715349bc8c542823fc9ef06d9d2a2dd6d8fa
jimtcp <32323239+jimtcp@users.noreply.github.com>
2021-06-08 10:53:21 +1000

    Update start_gpu_doge.sh

--- start_gpu_doge.sh
+++ start_gpu_doge.sh
@@ -1,1 +1,1 @@
-./PhoenixMiner -pool ethash.unmineable.com:3333 -wal DOGE:DAq95eRubEbSkav6qTET9bdfP7BuSc9JZt.$HOSTNAME#t2v4-ug5e
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal DOGE:DAq95eRubEbSkav6qTET9bdfP7BuSc9JZt.hostname#t2v4-ug5e
